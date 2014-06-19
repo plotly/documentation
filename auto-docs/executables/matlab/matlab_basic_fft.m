451248e38079f8217a7e445e72c79045ab70fc79
Chris P <christopher.parmer@gmail.com>
2014-06-19 15:32:13 -0400

    prerecognizing endotheliomas

---
+++ auto-docs/executables/matlab/matlab_basic_fft.m
@@ -0,0 +1,46 @@
+signin('theengineear', 'o9zlr0hy6z')
+
+% read audio text file  
+sigtext = urlread('https://raw.githubusercontent.com/plotly/documentation/reorganization/aux/fft-matlab'); 
+
+% conver to audio samples 
+sig = str2num(sigtext); 
+
+% sampling frequency
+fs = 44100; 
+
+% desired signal duration 
+dur = 1;
+
+% time axis vector
+t = linspace(0,dur,fs); 
+
+% fft length
+N = 4096; 
+
+% frequency axis vector 
+freq = linspace(0,fs,N); 
+
+% N-point fast fourier transform of signal
+F = fft(sig,N); 
+
+% Max frequency to visualize 
+maxFreq = N/16; %~2156 Hz. 
+
+% plot time domain waveform 
+fig = figure; 
+subplot(2,1,1)
+plot(t, sig)
+title('GUITAR C4 TEMPORAL/SPECTRAL VISUALIZATION'); 
+ylabel('AMPLITUDE'); 
+xlabel('TIME(s.)')
+
+% plot frequency fomain waveform
+subplot(2,1,2)
+plot(freq(1:maxFreq),abs(F(1:maxFreq))); 
+ylabel('MAGNITUDE'); 
+xlabel('FREQUENCY(Hz.)'); 
+
+% PLOTLY
+response = fig2plotly(fig,'strip',1);
+plotly_url = response.url;
