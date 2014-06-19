03a5f8b403dcc41e0d9735105b880b3b2921330e
Chris P <christopher.parmer@gmail.com>
2014-06-19 18:08:56 -0400

    file option etc examples

---
+++ auto-docs/executables/r/fileopt_example.r
@@ -0,0 +1,19 @@
+library(plotly)
+
+p <- plotly(username='TestBot', key='r1neazxo9w')
+
+trace1 <- list(
+  x = c(1, 2, 3, 4), 
+  y = c(10, 15, 13, 17), 
+  type = "scatter"
+)
+trace2 <- list(
+  x = c(1, 2, 3, 4), 
+  y = c(16, 5, 11, 9), 
+  type = "scatter"
+)
+data <- list(trace1, trace2)
+
+response <- p$plotly(data, kwargs=list(filename="fileopt-example", fileopt="overwrite", auto_open="FALSE"))
+url <- response$url
+filename <- response$filename
