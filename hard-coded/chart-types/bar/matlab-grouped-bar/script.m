9870d3656334949fe261315818c10876cf119835
Chris P <christopher.parmer@gmail.com>
2014-06-21 19:50:48 -0400

    no more "matlab syntax" verbage - matlab grouped bar chart is now pretty

--- hard-coded/chart-types/bar/matlab-grouped-bar/script.m
+++ hard-coded/chart-types/bar/matlab-grouped-bar/script.m
@@ -6,8 +6,9 @@ Y = [1.0 0.5 0.7
      4.0 4.0 4.5
      3.0 2.0 2.0];
 
+fig = figure;
 bar(Y,'group');
 
-% PLOTLY 
-response = fig2plotly();
+% PLOTLY
+response = fig2plotly(fig, 'strip', 1);
 plotly_url = response.url;
