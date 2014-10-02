8903b4d9d21f3ab37dcef7cc66afc39f517f9d4b
Chuck Bronson <chuck.james.bronson@gmail.com>
2014-10-02 00:20:16 -0400

    improved color and style of line/marker

--- hard-coded/chart-types/line_exclusive/matlab-line-style/script.m
+++ hard-coded/chart-types/line_exclusive/matlab-line-style/script.m
@@ -5,8 +5,13 @@ y1 = sin(x);
 y2 = sin(x-0.25);
 y3 = sin(x-0.5);
 
-fig = figure
-plot(x,y1,'g',x,y2,'b--o',x,y3,'c*')
+fig = figure;
+hold on
+
+plot(x,y1,'Color',[150 244 100]/255,'LineWidth',3,'LineStyle','-.');
+plot(x,y2,'Color',[201 24 100]/255,'LineWidth',3,'LineStyle','--');
+plot(x,y3,'Color',[21 24 250]/255,'LineWidth',2,'LineStyle','o');
+
 
 %--PLOTLY--%
 response = fig2plotly(fig, 'strip', false); % <----- preserve MATLAB style!
