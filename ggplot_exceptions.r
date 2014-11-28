ebca7f35d23466cf70fd881be6bba1d70dbaadaa
Marianne Corvellec <marianne.corvellec@ens-lyon.org>
2014-11-28 17:07:05 -0500

    Clean up and update R info (no impact)

--- ggplot_exceptions.r
+++ ggplot_exceptions.r
@@ -1,12 +1,12 @@
 library(RJSONIO)
-path = 'exceptions/ggplot2'
+path = "exceptions/ggplot2"
 trace = TRUE
 for (nm in list.files(path, pattern = "\\.[RrSsQq]$")) {
-   if(trace) cat(nm,":")
+   if (trace) cat(nm, ":")
    source(file.path(path, nm))
    # save plotly_url to file
    writeLines(toJSON(list(url=plotly_url)),
-              paste(path, '/', substr(basename(nm), 1, nchar(basename(nm)) - 2), '.json', sep=""))
+              paste(path, "/", substr(basename(nm), 1, nchar(basename(nm)) - 2), ".json", sep=""))
    rm(plotly_url)
-   if(trace) cat("\n")
+   if (trace) cat("\n")
 }
