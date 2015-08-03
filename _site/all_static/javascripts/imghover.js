1bb360b1801b75b4627ae8554919cdcd80b83a79
chriddyp <chris@plot.ly>
2015-08-03 15:56:16 -0400

    site update

--- _site/all_static/javascripts/imghover.js
+++
@@ -1,8 +0,0 @@
-function movingInImgOp(img, text) {
-		img.style.opacity = "0.2";
-		text.style.display = "";
-}
-function movingOutImgOp(img, text) {	
-		text.style.display = "none";
-		img.style.opacity = "1";
-}
