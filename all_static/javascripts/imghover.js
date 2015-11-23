003a8c54fb37ae03a5523a404efab16aa89ff842
jackparmer <jparmer09@gmail.com>
2015-11-22 22:26:17 -0500

    Site updated at 2015-11-23 02:59:33 UTC

--- all_static/javascripts/imghover.js
+++ all_static/javascripts/imghover.js
@@ -6,3 +6,18 @@ function movingOutImgOp(img, text) {
 		text.style.display = "none";
 		img.style.opacity = "1";
 }
+
+/* vertical position of side navigation on reference pages */
+var refBar = document.getElementById('reference-side-nav');
+var MAINBAR_H = 310;
+update_pos = function() {
+	if (document.body.scrollTop > MAINBAR_H ){
+		refBar.style.position = "fixed";
+		refBar.style.top = "0";
+	}
+	else{
+		refBar.style.position = "absolute";
+		refBar.style.top = MAINBAR_H;
+	}
+}
+document.addEventListener ("scroll", update_pos);
