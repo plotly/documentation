f8bf3a188368595a9acd9b03d777bf2423d78b5b
Kully <adam@plot.ly>
2018-08-15 10:38:12 -0400

    improve code logic incase multiple 10-color dflt lists are in schema

--- get_plotschema.py
+++ get_plotschema.py
@@ -4,19 +4,27 @@ Update _data/plotschema.json for rakefile.
 import requests
 
 plotschema = requests.get('https://api.plot.ly/v2/plot-schema/?sha1=%27%27',
-                          headers={'plotly-client-platform': 'python'},
-                          auth=('plotly_docs', 'ikwqmjam2o')).content
+                         headers={'plotly-client-platform': 'python'},
+                         auth=('plotly_docs', 'ikwqmjam2o')).content
 f = open('plotschema.json', 'w')
 
-# inject single 's around colorway dflt list
 n = 10  # number of colors in colorway
-pattern = '"dflt":\[' + '"#[0-z]+",[ ]*'*(n-1) + '"#[0-z]+"[ ]*\]'
-colorway = re.findall(pattern, plotschema)[0]
+pattern = '"dflt":[ ]*\[' + '"#[0-z]+",[ ]*'*(n-1) + '"#[0-z]+"[ ]*\]'
+colorway = re.findall(pattern, plotschema)
 
-colorway = colorway.replace('[', "\'[")
-colorway = colorway.replace(']', "]\'")
+for j in range(len(colorway)):
+    colorway[j] = colorway[j].replace('[', "\'[")
+    colorway[j] = colorway[j].replace(']', "]\'")
 
-split = re.split(pattern, plotschema)
-plotschema = split[0] + colorway + split[1]
-
-f.write(plotschema)
+print colorway
+new_plotschema = ''
+if len(colorway) > 0:
+    split = re.split(pattern, plotschema)
+    for _ in range(len(split) - 1):
+        new_plotschema += split[_]
+        new_plotschema += colorway[_]
+    new_plotschema += split[len(split) - 1]
+    
+else:    
+    new_plotschema = plotschema
+f.write(new_plotschema)
