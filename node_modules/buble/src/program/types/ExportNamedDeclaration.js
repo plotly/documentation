96a15ba60f628df97827fcde9f2c87ab3c42141e
Kully <adam@plot.ly>
2018-08-20 17:51:03 -0400

    Site updated at 2018-08-20 21:32:03 UTC

--- node_modules/buble/src/program/types/ExportNamedDeclaration.js
+++ node_modules/buble/src/program/types/ExportNamedDeclaration.js
@@ -2,9 +2,8 @@ import Node from '../Node.js';
 import CompileError from '../../utils/CompileError.js';
 
 export default class ExportNamedDeclaration extends Node {
-	initialise(transforms) {
-		if (transforms.moduleExport)
-			throw new CompileError('export is not supported', this);
-		super.initialise(transforms);
+	initialise ( transforms ) {
+		if ( transforms.moduleExport ) throw new CompileError( this, 'export is not supported' );
+		super.initialise( transforms );
 	}
 }
--- node_modules/buble/src/program/types/shared/ModuleDeclaration.js
+++ node_modules/buble/src/program/types/shared/ModuleDeclaration.js
@@ -2,9 +2,8 @@ import Node from '../../Node.js';
 import CompileError from '../../../utils/CompileError.js';
 
 export default class ModuleDeclaration extends Node {
-	initialise(transforms) {
-		if (transforms.moduleImport)
-			throw new CompileError('Modules are not supported', this);
-		super.initialise(transforms);
+	initialise ( transforms ) {
+		if ( transforms.moduleImport ) throw new CompileError( this, 'Modules are not supported' );
+		super.initialise( transforms );
 	}
 }
