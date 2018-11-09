8458640310936be82e58e57c07961f233f657525
michaelbabyn <41019918+michaelbabyn@users.noreply.github.com>
2018-11-09 14:21:24 -0500

    Update get_plotschema.py

--- get_plotschema.py
+++ get_plotschema.py
@@ -6,5 +6,5 @@ import requests
 plotschema = requests.get('https://api.plot.ly/v2/plot-schema/?sha1=%27%27',
                           headers={'plotly-client-platform': 'python'},
                           auth=('plotly_docs', 'ikwqmjam2o')).content
-f = open('_data/plotschema.json', 'w')
+f = open('_data/plotschema.json', 'wb')
 f.write(plotschema)
