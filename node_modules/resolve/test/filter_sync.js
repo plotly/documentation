0b8ac1c65266aceda0090c253c6fe0001df09ce3
Nicolas Kruchten <nicolas@plot.ly>
2019-07-08 14:50:08 -0400

    Site updated at 2019-07-08 18:47:24 UTC

--- node_modules/resolve/test/filter_sync.js
+++ node_modules/resolve/test/filter_sync.js
@@ -4,23 +4,13 @@ var resolve = require('../');
 
 test('filter', function (t) {
     var dir = path.join(__dirname, 'resolver');
-    var packageFilterArgs;
     var res = resolve.sync('./baz', {
         basedir: dir,
-        packageFilter: function (pkg, dir) {
+        packageFilter: function (pkg) {
             pkg.main = 'doom';
-            packageFilterArgs = [pkg, dir];
             return pkg;
         }
     });
-
-    t.equal(res, path.join(dir, 'baz/doom.js'), 'changing the package "main" works');
-
-    var packageData = packageFilterArgs[0];
-    t.equal(packageData.main, 'doom', 'package "main" was altered');
-
-    var packageFile = packageFilterArgs[1];
-    t.equal(packageFile, path.join(dir, 'baz'), 'second packageFilter argument is "dir"');
-
+    t.equal(res, path.join(dir, 'baz/doom.js'));
     t.end();
 });
