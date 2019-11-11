08e54bc8e1b0bc048591310df1ccd2b22ea46b96
CanerIrfanoglu <canerirfanoglu@gmail.com>
2019-11-11 18:16:35 -0500

    Ran examples 3d & animations

--- RdepLibs.R
+++ RdepLibs.R
@@ -1,1 +1,16 @@
 install.packages("plotly")
+# r/3d/3d-tri-surf/Helicopter
+install.packages("geomorph")
+# r/3d/3d-tri-surf/Maps -- Example still not functional contacted with creator of dependency package
+install.packages("rgl")
+install.packages("RTriangle")
+install.packages("graticule")
+devtools::install_github("r-gris/rangl")
+# r/3d/cone/3D Cone of Wind Dataset
+install.packages("rjson")
+# r/animations/2017-05-28-cumulative-animations/Cumulative Lines Animation
+install.packages("dplyr")
+# r/animations/2017-05-28-cumulative-animations/Filled-Area Animation
+install.packages("quantmod")
+# r/animations/2017-05-28-cumulative-animations/Mulitple Trace Animations
+install.packages("gapminder")
