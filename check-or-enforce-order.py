9e473787a8c086903a86033003cc62d4af8d92b7
Joseph Damiba <joseph.damiba@plot.ly>
2019-11-13 10:40:26 -0500

    reverting posts to original order
    refactoring CI scripts
    
    fixup to path in python docs
    
    fixup to scripts
    
    fixup
    
    fixup
    
    fixup

--- check-or-enforce-order.py
+++ check-or-enforce-order.py
@@ -2,6 +2,7 @@ import frontmatter
 from pathlib import Path, PosixPath
 import sys
 
+# path here is intended to include only posts from a single language
 try:
     file_path = str(sys.argv[1])
 except:
@@ -9,20 +10,25 @@ except:
 
 def enforceOrder(listToBeOrdered):
     for index, post in enumerate(listToBeOrdered):
+        if file_path == 'build/html':
+            if index+1 == 5:
+                continue
+
         if index+1 != post['order']:      
             postToBeAltered = frontmatter.load(post['path'])
             postToBeAltered.metadata['order'] = index+1
             frontmatter.dump(postToBeAltered, post['path'])
 
 def checkConsecutive(l): 
-        return sorted(l) == list(range(1, len(l)+1)) 
+    if len(order) != len(set(order)):
+        return False
+    
+    if sorted(l) == list(range(1, len(l)+1)):
+        return True
+    else:
+        return False
 
 categories = ["file_settings", "basic", "statistical", "scientific", "maps", "3d_charts", "multiple_axes"]
-
-try:
-    path = str(sys.argv[1])
-except:
-    raise Exception("You need to specify a path!")
 
 paths = []
 for suffix in ["md", "html"]:
@@ -38,21 +44,14 @@ for category in categories:
                 if post.metadata['display_as'] == category:
                     postFamily.append({'path':str(md_path), 'order' : post.metadata['order']})
                 
+    if file_path == 'build/html':
+        postFamily.append({'path':'placeholder', 'order' : 5 })
+
     sortedPostFamily = sorted(postFamily, key = lambda i: i['order'])
 
     order = [ p['order'] for p in sortedPostFamily ]
 
     print(order)
-
-    if order[0] != 1:
-        if len(sys.argv) == 3:
-            if sys.argv[2] == 'enforce':
-                print('Order Check Did Not Pass! ENFORCING CORRECT ORDER for {}'.format(category))
-                enforceOrder(sortedPostFamily)
-        else:
-            raise Exception("Order Check Failed! First post in {} display_as does not have order 1!".format(category))
-
-    
 
     if not checkConsecutive(order):
         if len(sys.argv) == 3:
@@ -60,15 +59,7 @@ for category in categories:
                 print('Order Check Did Not Pass! ENFORCING CORRECT ORDER for {}'.format(category))
                 enforceOrder(sortedPostFamily)
         else: 
-            raise Exception("Order Check Failed! Order front-matter in '{}' display_as are not consecutive integers!!".format(category))
+            raise Exception("Order Check in '{}' display_as!!".format(category))
 
-    if len(order) != len(set(order)):
-        if len(sys.argv) == 3:
-            if sys.argv[2] == 'enforce':
-                print('Order Check Did Not Pass! ENFORCING CORRECT ORDER for {}'.format(category))
-                enforceOrder(sortedPostFamily)
-        else:         
-            raise Exception("Order Check Failed! {} display_as has duplicate order frontmatter!!".format(category))
-
-    print("Order Checks Passed for {} display_as!".format(category))
+    print("Order Checks Passed for {} display_as in {}!".format(category, file_path))
     order = []
