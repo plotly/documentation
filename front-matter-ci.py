96d8356fc582491a0fc5a103a5833d5965147355
Joseph Damiba <joseph.damiba@plot.ly>
2019-11-15 15:37:53 -0500

    refactor ci scripts
    fixup
    
    refactoring ci scripts for performance and accuracy
    
    fixup
    
    fixup
    
    more refactoring
    
    remove anti-pattern where function returns different types
    
    adding check for lack of trailing slash on permalink
    
    removing unneeded else: continue
    
    more refactoring

--- front-matter-ci.py
+++ front-matter-ci.py
@@ -2,73 +2,102 @@ import frontmatter
 from pathlib import Path, PosixPath
 import sys
 
-# should be either '_posts' for this repo or 'build/html' for the py-docs repo
+# 'path' == '_posts' in 'documentation'
+# 'path' == 'build/html' in 'py-docs'
 try:
     path = str(sys.argv[1])
 except:
     raise Exception("You need to specify a path that contains the files with front matter.")
 
+def ci_check(checkList, error_message):
+    print("***********************************!")
+    print("Checking... {}".format(error_message))
+    if len(checkList) > 0:
+        print("Failed!")
+        print("List of failed permalinks:")
+        print(checkList)
+        print("\n")
+    else:
+        print("Passed!")
+
 paths = []
-for suffix in ["md", "hmtl"]:
+allPosts = []
+postsWithNoName = []
+postsWithTitle = []
+allPermalinks = []
+indexOverflow = []
+postsWithNoThumbnail = []
+temp = []
+duplicatePermalinks = []
+noTrailingSlash = []
+
+categories = ["file_settings", "basic", "financial", "statistical", "scientific", "maps", "3d_charts", "multiple_axes"]
+languages = ["python", "python/v3", "plotly_js", "r"]
+
+# collect all paths 
+for suffix in ["md", "html"]:
     paths += [x for x in Path(path).glob("**/*."+suffix)]
 
-# this will store the front matter for all posts in the given directory
-allPosts = [];
-
-#get all posts with frontmatter in md format
+# collect all posts
 for path in paths:
     post = frontmatter.load(str(path))
     if len(post.metadata.keys()) > 0:
         allPosts.append(post)
 
-#make sure that every post that is not a redirect has a name tag in the front matter
-noNamePaths = [];
-titlePaths = [];
-permalinks = [];
-
+# perform checks
 for post in allPosts:
-    if len(post.metadata.keys()) > 0:
-        meta = post.metadata
+    meta = post.metadata
+   
+    # ignore posts with 'jupyter' in front-matter
+    if "jupyter" in meta:
+        continue
 
-        #in case the front-matter format is different
-        if "jupyter" in meta:
-            continue
+    # Check #1 - do all non-redirect posts have names? 
+    if "name" not in meta and "redirect_to" not in meta:
+        postsWithNoName.append(post.metadata['permalink'])
 
-        # Check 1
-        if "name" not in meta and "redirect_to" not in meta:
-                noNamePaths.append(post.metadata)
+    # Check #2 - do any posts have titles?
+    if "title" in meta:
+        postsWithTitle.append(post.metadata['permalink'])
 
-        # Check 2
-        if "title" in meta:
-            titlePaths.append(post.metadata)
+    # Check #3 - are there duplicate permalinks/redirect_froms?
+    if "permalink" in meta and meta['permalink'] != '//plot.ly/products/dash/':
+        allPermalinks.append(meta['permalink'])
+    if "redirect_from" in meta:
+        allPermalinks.append(meta['redirect_from'])
+    
+    # Check #4 - are there posts with order > 5 and 'page_type: example_index'?
+    if "display_as" in meta and meta['display_as'] in categories:
+        if "language" in meta and meta['language'] in languages:
+            if "order" in meta and meta['order'] > 5:
+                if "page_type" in meta and meta['page_type'] == "example_index":
+                    indexOverflow.append(meta['permalink'])
+                        
+    # Check #5 - does every post have a thumbnail?
+    if "thumbnail" not in meta:
+        if "display_as" in meta and meta['display_as'] in categories:
+            if "language" in meta and meta['language'] in languages:
+                postsWithNoThumbnail.append(meta['permalink'])
+    
+    # Check #6 - do any permalinks not end with a trailing slash?
+    if "permalink" in meta:
+        if meta['permalink'][-1] != "/":
+            noTrailingSlash.append(meta['permalink'])
 
-        # Check 3 - ignor dash call outs
-        if "permalink" in meta and meta['permalink'] != '//plot.ly/products/dash/':
-            permalinks.append(meta['permalink'])
-        if "redirect_from" in meta:
-            permalinks.append(meta['redirect_from'])
 
-# Check 1
-if len(noNamePaths) > 0:
-    raise Exception("CI Check #1 Not Passed: post:\n'{}' is not a redirect but is missing a name frontmatter\n".format('\n'.join([str(item) for item in noNamePaths])))
-print("CI Check #1 Passed: All non-redirect posts have names!")
-
-# Check 2
-if len(titlePaths) > 0:
-    raise Exception("CI Check #2 Not Passed: post:\n'{}' has a title. Titles no longer needed!\n".format('\n'.join([str(item) for item in titlePaths])))
-print("CI Check #2 Passed: No posts have titles!")
-
-# Check 3
-temp = [];
-dupeLinks = [];
-
-for post in permalinks:
+for post in allPermalinks:
     if post in temp:
-        dupeLinks.append(post)
+        duplicatePermalinks.append(post)
         continue
     else:
         temp.append(post)
+        
+print("Begin CI Checks!\n")
+ci_check(postsWithNoName, "do all non-redirect posts have names?")
+ci_check(postsWithTitle, "do any posts have titles?")
+ci_check(indexOverflow, "are there posts with order > 5 and 'page_type: example_index'?")
+ci_check(duplicatePermalinks, "are there duplicate permalinks/redirect_froms?")
+ci_check(postsWithNoThumbnail, "does every post have a thumbnail?")
+ci_check(noTrailingSlash, "do any permalinks not end with a trailing slash?")
 
-if len(dupeLinks) > 0:
-    raise Exception("CI Check #3 Not Passed: Following permalinks:\n{} are duplicated!\n".format('\n'.join([str(item) for item in dupeLinks])))
-print("CI Check #3 Passed: No duplicate permalinks!")
+print("End CI Checks!\n")
