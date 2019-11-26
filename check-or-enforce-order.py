aa3f705ac428632801f98a65b39bfca66adcac6c
Joseph Damiba <joseph.damiba@plot.ly>
2019-11-26 11:47:02 -0500

    renaming file_path to folder_path for readability and using correct variable for .ipynb check

--- check-or-enforce-order.py
+++ check-or-enforce-order.py
@@ -6,7 +6,7 @@ import sys
 # _posts/r, _posts/plotly_js, _posts/python-v3, _posts/python in 'documentation'
 # build/html in 'plotly.py-docs'
 try:
-    file_path = str(sys.argv[1])
+    folder_path = str(sys.argv[1])
 except:
     raise Exception("You need to specify a path!")
 
@@ -32,7 +32,7 @@ def enforceOrder(list_to_be_ordered):
     print(list_to_be_ordered)
     for index, post in enumerate(list_to_be_ordered):
         post_to_be_altered = fm.load(post)
-        if file_path == "_posts/r": # accounts for the fact that sometimes there are both .md and .Rmd files
+        if folder_path == "_posts/r": # accounts for the fact that sometimes there are both .md and .Rmd files
             if post[-3:] == ".md":
                 post_to_be_altered.metadata['order'] = index+1
                 fm.dump(post_to_be_altered, post)
@@ -43,7 +43,7 @@ def enforceOrder(list_to_be_ordered):
                     fm.dump(rpost_to_be_altered, rPath)
                 except:
                     continue
-        elif file_path == "python": # accounts for the fact that this is also run in the plotly.py-docs repo
+        elif folder_path == "python": # accounts for the fact that this is also run in the plotly.py-docs repo
             post_to_be_altered.metadata["jupyter"]["plotly"]['order'] = (index+2 if index>=4 else index+1)
             fm.dump(post_to_be_altered, post)
         else:        
@@ -51,7 +51,7 @@ def enforceOrder(list_to_be_ordered):
             fm.dump(post_to_be_altered, post)
 
 def is_consecutive(list_to_be_checked): 
-    if file_path in ["python", "build/html"]:
+    if folder_path in ["python", "build/html"]:
         list_to_be_checked = list_to_be_checked + [5]
     print(sorted(list_to_be_checked))
     return sorted(list_to_be_checked) == list(range(1, len(list_to_be_checked)+1))
@@ -69,8 +69,8 @@ def validate_front_matter(front_matter):
 def get_paths_and_orders_by_category():
     posts_by_category = {category: dict(orders=[], paths=[]) for category in categories}
     for suffix in ["md", "html"]:
-        for path in Path(file_path).glob("**/*."+suffix): 
-            if ".ipynb_checkpoints" not in str(file_path):
+        for path in Path(folder_path).glob("**/*."+suffix): 
+            if ".ipynb_checkpoints" not in str(path):
                 post = get_post(path)
                 front_matter = get_front_matter(post)
                 if "display_as" in front_matter:
@@ -93,19 +93,19 @@ def check_order():
                 print("ENFORCING CORRECT ORDER! for {}\n".format(category))
                 enforceOrder(sorted_paths)
             else:
-                arg = file_path if file_path != "build/html" else "python"
+                arg = folder_path if folder_path != "build/html" else "python"
                 raise Exception("Order is not sequential! **CHECK NOT PASSED** in '{}' display_as! Run 'python check-or-enforce-order.py {} enforce' to resolve!".format(category, arg))
         else:
             print("*Check Passed!*\n")
 
 print("**********************************************")
-print("Order of '{}' Before Enforcing!".format(file_path))
+print("Order of '{}' Before Enforcing!".format(folder_path))
 print("**********************************************\n")
 
 check_order()
 
 if enforce is True:
     print("*******************************************")
-    print("Order of '{}' After Enforcing!".format(file_path))
+    print("Order of '{}' After Enforcing!".format(folder_path))
     print("*******************************************\n")
     check_order()
